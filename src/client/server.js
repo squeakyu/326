592d0011ede979c5c2ba0e723d15044eea9c0043
botprotection <steventdev@gmail.com>
2024-05-14 19:12:56 -0400

    Express

--- src/client/server.js
+++ src/client/server.js
@@ -1,12 +1,28 @@
 import express from "express";
 import morgan from 'morgan';
 
-
 const app = express();
 const port = 3260;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'GET request received' });
+});
+
+app.post('/api/data', (req, res) => {
+  res.json({ message: 'POST request received' });
+});
+
+app.put('/api/data/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ message: `PUT request received for ID ${id}` });
+});
+
+app.use('/api/fetch', (req, res) => {
+  res.json({ message: 'FETCH request received' });
+});
 
 app.get('/', (req, res) => {
   res.sendFile('login.html', { root: 'src/client' });
